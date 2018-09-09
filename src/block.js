c4e82866def319d1506375d1fa2dfbba266bbcb1
ViziosDe <jonas.roller@hotmail.com>
2018-09-09 21:36:12 +0200

    initial commit

---
+++ src/block.js
@@ -0,0 +1,50 @@
+const SHA256 = require('crypto-js/sha256')
+
+class Block {
+
+  constructor(input, output, signatures, nonce, p_o_w) {
+    this.input = input;
+    this.output = output;
+    this.signatures = signatures;
+    this.nonce = nonce;
+    this.p_o_w = p_o_w;
+  }
+
+  calculateHash() {
+    return SHA256(this.transaction_hash + this.input + this.output + this.signatures + this.previous_hash + this.nonce + this.p_o_w).toString();
+  }
+
+}
+
+
+class Input {
+
+  constructor(transaction_hash, output_index) {
+    this.transaction_hash = transaction_hash;
+    this.output_index = output_index;
+  }
+
+}
+
+/*
+ * class representing an output object.
+ * 3 arguments arr of receiver -> value pairs.
+ * a pair consists of : public key,
+ *
+ */
+class Output {
+
+  constructor(sender_id, receiver_id, amount) {
+    this.sender_id = sender_id;
+    this.receiver_id = receiver_id;
+    this.amount = amount;
+  }
+
+}
+
+
+module.exports = {
+  Block,
+  Input,
+  Output
+};
---
+++ src/blockchain.js
@@ -0,0 +1,45 @@
+const {Block, Input, Output}  = require('./block');
+
+class Blockchain {
+
+  constructor() {
+    this.chain = [new Block('0',{},{},{},'0','0','0')];
+  }
+
+  latestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock) {
+    newBlock.previousHash = this.latestBlock().hash;
+    newBlock.hash = newBlock.calculateHash();
+    this.chain.push(newBlock);
+  }
+
+  checkValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+
+      if (currentBlock.hash !== currentBlock.calculateHash()) {
+        return false;
+      }
+
+      if (currentBlock.previousHash !== previousBlock.hash) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+}
+
+let jsChain = new Blockchain();
+jsChain.addBlock(new Block({amount: 5},{amount:5},'asda123',0,true));
+jsChain.addBlock(new Block({amount: 15},{amount:15},'asda123',20,true));
+
+console.log(JSON.stringify(jsChain, null, 4));
+console.log("Is blockchain valid? " + jsChain.checkValid());
+
+module.exports = Blockchain;
