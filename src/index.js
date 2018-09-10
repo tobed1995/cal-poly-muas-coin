6691b7e28898e36c3ba69b524319414d5ac232fd
ViziosDe <jonas.roller@hotmail.com>
2018-09-10 02:59:20 +0200

    minor changes

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ const defaultsDeep = require('@nodeutils/defaults-deep')
 const waterfall = require('async/waterfall')
 const parallel = require('async/parallel')
 const series = require('async/series')
+
+
 const UnverifiedTransactionPool = require('./unverifiedTransactionPool')
 const Blockchain = require('./blockchain')
 const Transaction = require('./transaction')
@@ -18,100 +20,26 @@ const Output = require('./output')
 
 let chain = new Blockchain();
 
-class MyBundle extends libp2p {
-  constructor (_options) {
-    const defaults = {
-      modules: {
-        transport: [ TCP ],
-        streamMuxer: [ Mplex ],
-        connEncryption: [ SECIO ],
-        peerDiscovery: [ MulticastDNS ]
-      },
-      config: {
-        peerDiscovery: {
-          mdns: {
-            interval: 2000,
-            enabled: true
-          }
-        },
-        EXPERIMENTAL: {
-          pubsub: true
-        }
-      }
-    }
+const pool = new UnverifiedTransactionPool();
 
-    super(defaultsDeep(_options, defaults))
-  }
-}
+var input = [];
+//
+input.push(new Input('0xblub', 2));
 
-function createNode (callback) {
-  let node
+var output = [];
+output.push(new Output('234', 4));
+pool.pushTransaction(new Transaction(input, output, null));
+pool.pushTransaction(new Transaction(input, output, null));
+pool.pushTransaction(new Transaction(input, output, null));
+console.log(JSON.stringify(pool.getPool(), null, 4));
+pool.getRandomTransaction();
+input.push(new Input('234', 5));
+output.push(new Output('234', 4));
+output.push(new Output('234', 4));
+output.push(new Output('234', 4));
+var trans = new Transaction(input, output, null);
+chain.verifyTransaction(trans);
+pool.pushTransaction(trans);
 
-  waterfall([
-    (cb) => PeerInfo.create(cb),
-    (peerInfo, cb) => {
-      peerInfo.multiaddrs.add('/ip4/0.0.0.0/tcp/0')
-      node = new MyBundle({
-        peerInfo
-      })
-      node.start(cb)
-    }
-  ], (err) => callback(err, node))
-}
-
-parallel([
-  (cb) => createNode(cb),
-  (cb) => createNode(cb)
-], (err, nodes) => {
-  if (err) { throw err }
-
-  const node1 = nodes[0]
-  const node2 = nodes[1]
-
-  const pool = new UnverifiedTransactionPool();
-
-  var input = [];
-  //
-  input.push(new Input('0xblub', 2));
-
-
-  var output = [];
-  output.push(new Output('234', 4));
-  pool.pushTransaction(new Transaction(input, output, null));
-  pool.pushTransaction(new Transaction(input, output, null));
-  pool.pushTransaction(new Transaction(input, output, null));
-  console.log(JSON.stringify(pool.getPool(), null, 4));
-  pool.getRandomTransaction();
-  input.push(new Input('234', 5));
-  output.push(new Output('234', 4));
-  output.push(new Output('234', 4));
-  output.push(new Output('234', 4));
-  var trans = new Transaction(input, output, null);
-  chain.verifyTransaction(trans);
-  pool.pushTransaction(trans);
-
-  console.log(JSON.stringify(pool.getPool(), null, 4));
-  // TODO: Broadcast the new Transaction!
-
-  series([
-    (cb) => node1.once('peer:discovery', (peer) => node1.dial(peer, cb)),
-    (cb) => setTimeout(cb, 500)
-  ], (err) => {
-    if (err) { throw err }
-
-    // Subscribe to the topic 'news'
-    node1.pubsub.subscribe('news',
-      (msg) => console.log(msg.from, msg.data.toString()),
-      () => {
-        setInterval(() => {
-          // Publish the message on topic 'news'
-          node2.pubsub.publish(
-            'news',
-            Buffer.from('Bird bird bird, bird is the word!'),
-            () => {}
-          )
-        }, 1000)
-      }
-    )
-  })
-})
+console.log(JSON.stringify(pool.getPool(), null, 4));
+// TODO: Broadcast the new Transaction!
---
+++ src/muasCoin.js
@@ -0,0 +1,78 @@
+// Creating a bundle that adds:
+//   transport: websockets + tcp
+//   stream-muxing: spdy & mplex
+//   crypto-channel: secio
+//   discovery: multicast-dns
+
+const libp2p = require('libp2p')
+const TCP = require('libp2p-tcp')
+const WS = require('libp2p-websockets')
+const SPDY = require('libp2p-spdy')
+const MPLEX = require('libp2p-mplex')
+const SECIO = require('libp2p-secio')
+const MulticastDNS = require('libp2p-mdns')
+const DHT = require('libp2p-kad-dht')
+const defaultsDeep = require('@nodeutils/defaults-deep')
+const Protector = require('libp2p-pnet')
+
+class muasNode extends libp2p {
+  constructor (_options) {
+    const defaults = {
+      // The libp2p modules for this libp2p bundle
+      modules: {
+        transport: [
+          TCP,
+          new WS()                    // It can take instances too!
+        ],
+        streamMuxer: [
+          SPDY,
+          MPLEX
+        ],
+        connEncryption: [
+          SECIO
+        ],
+        // Encryption for private networks. Needs additional private key to work
+        // connProtector: new Protector(/*protector specific opts*/),
+        peerDiscovery: [
+          MulticastDNS
+        ],
+        dht: DHT                      // DHT enables PeerRouting, ContentRouting and DHT itself components
+      },
+
+      // libp2p config options (typically found on a config.json)
+      config: {                       // The config object is the part of the config that can go into a file, config.json.
+        peerDiscovery: {
+          mdns: {                     // mdns options
+            interval: 1000,           // ms
+            enabled: true
+          },
+          webrtcStar: {               // webrtc-star options
+            interval: 1000,           // ms
+            enabled: false
+          }
+          // .. other discovery module options.
+        },
+        relay: {                      // Circuit Relay options
+          enabled: false,
+          hop: {
+            enabled: false,
+            active: false
+          }
+        },
+        dht: {
+          kBucketSize: 20
+        },
+        // Enable/Disable Experimental features
+        EXPERIMENTAL: {               // Experimental features ("behind a flag")
+          pubsub: false,
+          dht: false
+        }
+      }
+    }
+
+    // overload any defaults of your bundle using https://github.com/nodeutils/defaults-deep
+    super(defaultsDeep(_options, defaults))
+  }
+}
+
+// Now all the nodes you create, will have TCP, WebSockets, SPDY, MPLEX, SECIO and MulticastDNS support.
