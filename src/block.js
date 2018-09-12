e3556ea20fd6bfe603689c3eb8f685f8f42105d9
Sebastian <sese.hartl@gmail.com>
2018-09-11 17:28:30 -0700

    signature verification implemented

--- src/block.js
+++ src/block.js
@@ -2,46 +2,49 @@ const SHA256 = require('crypto-js/sha256')
 
 class Block {
 
-  constructor(transaction, previousBlock) {
-    if (Array.isArray(transaction)) {
-      this.transaction = transaction;
-    } else {
-      this.transaction = [];
-      this.transaction.push(transaction);
+    constructor(transaction, previousBlock) {
+        if (Array.isArray(transaction)) {
+            this.transaction = transaction;
+        } else {
+            this.transaction = [];
+            this.transaction.push(transaction);
+        }
+        this.previousBlock = previousBlock;
+        this.nonce = null;
+        this.blockHash = null;
     }
-    this.previousBlock = previousBlock;
-    this.nonce = null;
-    this.blockHash = null;
-  }
 
-  getTransactions() {
-    return this.transaction;
-  }
-  getPreviousBlock() {
-    return this.previousBlock;
-  }
-  getNonce() {
-    return this.nonce;
-  }
-  getBlockHash() {
-    return this.blockHash;
-  }
-
-  setNonce(nonce) {
-    if (this.nonce === null) {
-      this.nonce = nonce;
+    getTransactions() {
+        return this.transaction;
     }
-  }
 
-  setBlockHash(hash) {
-    if (this.blockHash === null) {
-      this.blockHash = hash;
+    getPreviousBlock() {
+        return this.previousBlock;
     }
-  }
 
-  calculateHash() {
-    return SHA256(this.transaction + this.previousBlock + this.nonce).toString();
-  }
+    getNonce() {
+        return this.nonce;
+    }
+
+    getBlockHash() {
+        return this.blockHash;
+    }
+
+    setNonce(nonce) {
+        if (this.nonce === null) {
+            this.nonce = nonce;
+        }
+    }
+
+    setBlockHash(hash) {
+        if (this.blockHash === null) {
+            this.blockHash = hash;
+        }
+    }
+
+    calculateHash() {
+        return SHA256(this.transaction + this.previousBlock + this.nonce).toString();
+    }
 
 }
 
